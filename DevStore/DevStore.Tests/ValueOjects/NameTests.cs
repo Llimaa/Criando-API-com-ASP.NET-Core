c17949abcb01567f41eb8643b6aa845f087cbeb4
Marcos Lima de Fatima <marcoslimadefatima@gmial.com>
2019-06-09 01:05:32 -0300

    testando VO Name

---
+++ DevStore/DevStore.Tests/ValueOjects/NameTests.cs
@@ -0,0 +1,22 @@
+
+using DevStore.Domain.StoreContext.ValueObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class NameTests
+{
+
+    [TestMethod]
+    public void ShoulReturnNotificationWhenNameIsValid()
+    {
+        var name = new Name("Marcos", "Lima");
+        Assert.AreEqual(true, name.IsValid);
+    }
+
+    [TestMethod]
+    public void ShoulReturnNotificationWhenNameIsNotValid()
+    {
+        var name = new Name("","lima");
+        Assert.AreEqual(false, name.IsValid);
+    }
+}
